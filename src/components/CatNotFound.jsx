17033c8e91fe6ec0a38c3b717bacd7d2a29016f6
Justine George <justinegeo96@gmail.com>
2024-02-07 09:21:54 -0600

    Add useState hook to CatNotFound component

--- src/components/CatNotFound.jsx
+++ src/components/CatNotFound.jsx
@@ -1,15 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import defaultNotFoundImage from "@/assets/cat_not_working_1.jpeg";
 import { notFoundImages } from "@/site-config";
 
 export default function CatNotFound() {
-	const catImg =
-		notFoundImages[Math.floor(Math.random() * notFoundImages.length)] || defaultNotFoundImage;
-	console.log(catImg.src);
+	const initialCatImgSrc =
+        notFoundImages[Math.floor(Math.random() * notFoundImages.length)] || defaultNotFoundImage;
+    const [catImgSrc, setCatImgSrc] = useState(initialCatImgSrc.src);
 
 	return (
 		<img
-			src={catImg.src}
+			src={catImgSrc}
 			className={"h-64 w-full rounded-lg object-cover object-center"}
 			alt="A confused cat stopped working, indicating its confused about the current page"
 		/>
