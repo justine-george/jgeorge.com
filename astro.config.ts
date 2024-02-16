f6191378ea4739fc37f8cf9daf704e7331fb0e47
Justine George <justinegeo96@gmail.com>
2024-02-16 15:00:17 -0600

    Add astro-expressive-code package and configuration

--- astro.config.ts
+++ astro.config.ts
@@ -8,57 +8,75 @@ import remarkUnwrapImages from "remark-unwrap-images";
 import rehypeExternalLinks from "rehype-external-links";
 import { remarkReadingTime } from "./src/utils/remark-reading-time";
 import icon from "astro-icon";
+import astroExpressiveCode from 'astro-expressive-code'
 
 // https://astro.build/config
 export default defineConfig({
-	// ! Please remember to replace the following site property with your own domain
-	site: "https://www.jgeorge.dev/",
-	markdown: {
-		remarkPlugins: [remarkUnwrapImages, remarkReadingTime],
-		rehypePlugins: [
-			[rehypeExternalLinks, { target: "_blank", rel: ["nofollow, noopener, noreferrer"] }],
-		],
-		remarkRehype: { footnoteLabelProperties: { className: [""] } },
-		shikiConfig: {
-			theme: "github-dark",
-			wrap: false,
-		},
-	},
-	integrations: [
-		mdx({}),
-		tailwind({
-			applyBaseStyles: false,
-		}),
-		sitemap(),
-		react(),
-		icon(),
-	],
-	image: {
-		domains: ["webmention.io"],
-	},
-	// https://docs.astro.build/en/guides/prefetch/
-	prefetch: true,
-	vite: {
-		plugins: [rawFonts([".ttf"])],
-		optimizeDeps: {
-			exclude: ["@resvg/resvg-js"],
-		},
-	},
+  // ! Please remember to replace the following site property with your own domain
+  site: "https://www.jgeorge.dev/",
+  markdown: {
+    remarkPlugins: [remarkUnwrapImages, remarkReadingTime],
+    rehypePlugins: [[rehypeExternalLinks, {
+      target: "_blank",
+      rel: ["nofollow, noopener, noreferrer"]
+    }]],
+    remarkRehype: {
+      footnoteLabelProperties: {
+        className: [""]
+      }
+    },
+    // shikiConfig: {
+    //   theme: "dracula",
+    //   wrap: false
+    // }
+  },
+  integrations: [
+    astroExpressiveCode({
+      themes: ['github-dark', 'github-light'],
+      frames: {
+        // showCopyToClipboardButton: false,
+      },
+      styleOverrides: {
+        borderRadius: '0rem',
+        frames: {
+          shadowColor: 'bg',
+        },
+      },
+    }),
+    mdx({}), 
+    tailwind({
+      applyBaseStyles: false
+    }), 
+    sitemap(), 
+    react(), 
+    icon(), 
+    
+  ],
+  image: {
+    domains: ["webmention.io"]
+  },
+  // https://docs.astro.build/en/guides/prefetch/
+  prefetch: true,
+  vite: {
+    plugins: [rawFonts([".ttf"])],
+    optimizeDeps: {
+      exclude: ["@resvg/resvg-js"]
+    }
+  }
 });
-
 function rawFonts(ext: Array<string>) {
-	return {
-		name: "vite-plugin-raw-fonts",
-		// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-		// @ts-ignore:next-line
-		transform(_, id) {
-			if (ext.some((e) => id.endsWith(e))) {
-				const buffer = readFileSync(id);
-				return {
-					code: `export default ${JSON.stringify(buffer)}`,
-					map: null,
-				};
-			}
-		},
-	};
+  return {
+    name: "vite-plugin-raw-fonts",
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore:next-line
+    transform(_, id) {
+      if (ext.some(e => id.endsWith(e))) {
+        const buffer = readFileSync(id);
+        return {
+          code: `export default ${JSON.stringify(buffer)}`,
+          map: null
+        };
+      }
+    }
+  };
 }
