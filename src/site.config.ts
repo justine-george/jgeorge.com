efe70a5a059bd1981753923536cfab1527789080
Justine George <justinegeo96@gmail.com>
2024-08-12 01:55:37 -0700

    Update content

--- src/site.config.ts
+++ src/site.config.ts
@@ -12,6 +12,7 @@ import cat9 from "./assets/cat_not_working_9.jpeg";
 import tabkeeperThumbnail from "/thumbnails/tab-keeper-thumbnail.png?url";
 import bookfellowThumbnail from "/thumbnails/bookfellow-thumbnail.png?url";
 import planetThumbnail from "/thumbnails/planet-thumbnail.png?url";
+import nexusThumbnail from "/thumbnails/nexus-thumbnail.png?url";
 
 export const siteConfig: SiteConfig = {
 	// Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
@@ -102,44 +103,46 @@ export const experiences: Array<Experience> = [
 		// description: "Content Library Team — Resi On Demand.",
 		description:
 			// "In the Content Library Team for Resi On Demand, I developed scalable REST APIs, enabling enhanced access for over 5,000 churches to digital content. I implemented a monitoring solution on Google Cloud Platform using Terraform, which automates Slack notifications for video upload discrepancies, thus optimizing system alertness. Additionally, I directed a research project on converting MP4 videos to M4A audio formats with FFmpeg, guiding the roadmap for an audio-only download functionality.",
-			"At Resi Media's Content Library Team, I engineered Spring Boot RESTful APIs to streamline content access for over 5,000 non-profit orgs, managing 1 million metadata requests monthly. I developed a GCP-based alert system with Terraform, analyzing over 1,000 video uploads weekly for improved system alerts via Slack. Additionally, I led technical research on extracting M4A audio from DASH MP4 streams with FFmpeg, enabling single-click audio downloads from the video archive.",
+			"As an intern on the Content Library Team for Resi On Demand, I developed scalable RESTful APIs using Spring Boot to support non-profit organizations globally. I improved system performance by implementing a Terraform-automated GCP alert system, reducing anomaly response time by 40%. Additionally, I designed an FFmpeg-based feature for the Content Library, which enhanced user accessibility by enabling efficient audio extraction from video streams.",
 		technologies: [
 			"Java",
-			"Spring Boot",
+			"Spring",
 			"Docker",
-			"GCP (Pub/Sub)",
-			"Redis",
+			"GCP",
+			"Terraform",
 			"Cassandra",
+			"Redis",
+			"New Relic",
 			"TestNG",
-			"Node.js",
-			"TypeScript",
+			"FFmpeg",
+			"DASH",
 		],
 		links: {
 			company: "https://resi.io/resi-on-demand/",
 		},
 	},
 	{
-		title: "Software Development Engineer (Full Stack)",
+		title: "Software Development Engineer (Full-Stack)",
 		company: "Oracle",
 		// location: "Bengaluru, India", // ? Maybe add this back in later
 		startDate: new Date("2019-08-12"),
 		endDate: new Date("2022-07-16"),
 		// description: "Interest and Charges Team — Oracle FLEXCUBE Universal Banking",
 		description:
-			"In the Interest and Charges Team for Oracle FLEXCUBE Universal Banking, I built more than 30 RESTful APIs using Spring Boot and Spring Batch, improving service for over 900 global banks. I conducted over 350 code reviews, collaborating with cross-functional teams to enhance code reliability and product functionality, resulting in a 20% reduction in technical debt. Additionally, I engineered a Python-based automation tool for SQL entry generation, significantly optimizing database updates for a crucial financial module and reducing processing time by 95%. This tool was adopted across multiple teams. By optimizing our app's architecture, I reduced its Docker image size by 40%, enhancing deployment speed. Furthermore, I developed responsive UI components for a React-based SPA, increasing our page load speed by 60% and significantly boosting system performance.",
+			"As a member of the Interest and Charges Team for Oracle FLEXCUBE Universal Banking, I engineered over 30 RESTful API endpoints and Spring Batch processes, enhancing the core banking platform's efficiency for global financial institutions. I developed a Python CLI tool that accelerated Flyway SQL migration script creation by 95% and improved page load times by 60% through UI component optimization. Additionally, I streamlined app architecture, reducing Docker image size by 40%, and conducted over 350 code reviews, resulting in a 20% reduction in technical debt and enhanced overall product functionality.",
 		// "In the Interest and Charges Team for Oracle FLEXCUBE Universal Banking, I built more than 30 RESTful APIs using Spring Boot and Spring Batch, improving service for over 900 global banks. I engineered a Python-based automation tool for SQL entry generation, significantly optimizing database updates for a crucial financial module, reducing processing time by 95%. This tool was adopted across cross-functional teams. By optimizing our app's architecture, I reduced its Docker image size by 40%, enhancing deployment speed. Additionally, I developed responsive UI components for a React-based SPA that increased our page load speed by 60%, significantly boosting system performance.",
 		// "In Oracle's Interest and Charges Team, I developed REST APIs and a Python automation tool, streamlining operations for 900+ banks. Optimized application architecture, reducing Docker size by 40% and improving page load times by 60%.",
 		technologies: [
 			"Java",
-			"Spring Boot",
-			"Spring Batch",
-			"Docker",
-			"Redis",
+			"Spring",
 			"Python",
-			"JUnit",
+			"Docker",
 			"SQL",
 			"Oracle",
-			"Swagger",
+			"Redis",
+			"JUnit",
+			"Mockito",
+			"Selenium",
 			"React",
 			"JavaScript",
 		],
@@ -156,15 +159,15 @@ export const experiences: Array<Experience> = [
 		// description: "Storimart Team — B2B E-commerce Platform",
 		description:
 			// "In the Storimart Team for the B2B E-commerce Platform at Storilabs, as a Software Developer Intern focused on Android, I built a native app to enhance wholesaler-retailer interactions and purchasing processes. By integrating caching with OkHttp Interceptor and enabling local data storage with Room, I improved the app's performance and introduced offline functionality for the product catalog page, streamlining user access and transactions.",
-			"At Storilabs, I created a native Android app for B2B communication, enhancing wholesaler-retailer transactions. Implemented caching and offline capabilities, significantly boosting app performance and user experience.",
+			"During my internship at Storilabs, I developed a native Android app designed for B2B communication, which streamlined transactions between wholesalers and retailers. I implemented caching and offline capabilities, leading to a significant improvement in app performance and user experience.",
 		technologies: [
 			"Java",
 			"Retrofit",
-			"Firebase",
-			"SQlite (Room)",
 			"OkHttp",
+			"Firebase Cloud Messaging",
+			"Room",
+			"SQLite",
 			"XML",
-			"Material Design",
 		],
 		links: {
 			company: "https://storimart.com/",
@@ -174,21 +177,34 @@ export const experiences: Array<Experience> = [
 
 export const projects: Array<Project> = [
 	{
-		title: "Book Fellow: For Bibliophiles",
+		title: "Nexus - Decentralized Messaging Service",
 		description:
-			"A social media platform for book lovers, enabling users to write reviews, curate favorite lists, and follow the reading activities of fellow readers.",
+			"A decentralized messaging service that ensures secure, peer-to-peer communication with end-to-end encryption. Built using Go and WebRTC, Nexus offers a scalable infrastructure for seamless and private messaging.",
+		links: {
+			github: "https://github.com/justine-george/nexus-decentralized-messaging",
+			// github: "https://github.com/justine-george/book-fellow-backend-spring",
+			demo: "",
+		},
+		technologies: ["Go", "WebRTC", "Docker", "gRPC", "Kubernetes", "TLS"],
+		isFeatured: true,
+		thumbnail: nexusThumbnail,
+	},
+	{
+		title: "Book Fellow - Social Media Web App",
+		description:
+			"A social media platform designed for book enthusiasts, Book Fellow allows users to write detailed reviews, create and share personalized reading lists, and track the reading activities of others in the community.",
 		links: {
 			github: "https://github.com/justine-george/book-fellow-backend-spring",
 			demo: "",
 		},
-		technologies: ["Java", "PostgreSQL", "Go", "Docker", "AWS (EC2, ElastiCache, SNS)"],
+		technologies: ["Java", "Spring", "PostgreSQL", "Next.js", "AWS (EC2, S3, SNS, Lambda)"],
 		isFeatured: true,
 		thumbnail: bookfellowThumbnail,
 	},
 	{
-		title: "Tab Keeper (Featured on Web Store)",
+		title: "Tab Keeper - Chrome Extension",
 		description:
-			"Chrome extension for efficient tab management and cross-device syncing between Chrome browsers on desktop devices.",
+			"A Chrome extension for streamlined tab management and cross-device synchronization, enabling users to effortlessly manage and access their open tabs across multiple desktop devices.",
 		links: {
 			github: "https://github.com/justine-george/tab-keeper-react-chrome-extension",
 			storepage:
@@ -198,25 +214,25 @@ export const projects: Array<Project> = [
 		isFeatured: true,
 		thumbnail: tabkeeperThumbnail,
 		metrics: {
-			users: 531,
+			users: 1600,
 		},
 	},
 	{
 		title: "Planet: Event Planner App",
 		description:
-			"An event planning platform built with Next.js, facilitating seamless event organization and providing role-specific access for users and administrators.",
+			"An event planning platform developed with Next.js, Planet streamlines event organization by providing intuitive role-based access for users and administrators, ensuring a smooth and efficient planning process.",
 		links: {
 			github: "https://github.com/justine-george/planet-nextjs",
 			demo: "",
 		},
 		technologies: ["Next.js", "React", "DynamoDB", "GraphQL", "Docker", "AWS (Amplify, SQS)"],
-		isFeatured: true,
+		isFeatured: false,
 		thumbnail: planetThumbnail,
 	},
 	{
 		title: "Multi-threaded Simulation of a Tutoring Center with Priority Scheduling",
 		description:
-			"Multithreaded simulation of a tutoring center coordination system using POSIX threads, mutex locks, and semaphores in C.",
+			"A multithreaded simulation of a tutoring center's coordination system using POSIX threads, mutex locks, and semaphores in C, designed to manage scheduling and prioritize tutoring sessions effectively.",
 		links: {
 			github: "https://github.com/justine-george/Seeking-Tutor-Problem-POSIX-Threads",
 			demo: "",
@@ -227,7 +243,7 @@ export const projects: Array<Project> = [
 	{
 		title: "Lottery Scheduler Implementation in xv6",
 		description:
-			"Enhanced xv6 operating system kernel with a lottery scheduler, offering equitable CPU resource distribution through randomized process selection.",
+			"An enhancement to the xv6 operating system kernel introducing a lottery scheduler. This implementation improves CPU resource distribution through randomized process selection, ensuring fair and efficient resource allocation.",
 		links: {
 			github: "https://github.com/justine-george/Lottery-Scheduling-XV6",
 			demo: "",
@@ -237,7 +253,7 @@ export const projects: Array<Project> = [
 	{
 		title: "URL Shortener Microservice",
 		description:
-			"Node.js and MongoDB based microservice for URL shortening, offering JSON responses and original URL redirection.",
+			"A microservice for URL shortening built with Node.js and MongoDB, featuring JSON responses and seamless redirection from shortened URLs to their original destinations.",
 		links: {
 			github: "https://github.com/justine-george/url-shortener-service",
 			demo: "",
@@ -247,7 +263,7 @@ export const projects: Array<Project> = [
 	{
 		title: "8 Men Morris Game AI: MiniMax vs Alpha-Beta Pruning",
 		description:
-			"Implementation of the 8 Men Morris game using MiniMax and Alpha-Beta pruning algorithms, demonstrating AI strategies in game theory.",
+			"An AI implementation for the 8 Men Morris game utilizing MiniMax and Alpha-Beta pruning algorithms, showcasing advanced game theory strategies to enhance decision-making in gameplay.",
 		links: {
 			github: "https://github.com/justine-george/morris-game-variant-project",
 			demo: "",
@@ -257,7 +273,7 @@ export const projects: Array<Project> = [
 	{
 		title: "Customer Churn Prediction Using Machine Learning and Data Analysis",
 		description:
-			"Machine learning project for predicting telco customer churn, featuring extensive EDA, class imbalance handling, and multiple algorithm evaluations.",
+			"A machine learning project focused on predicting customer churn for a telecommunications company. This project includes extensive exploratory data analysis, handling of class imbalance, and evaluation of various predictive algorithms.",
 		links: {
 			github: "https://github.com/justine-george/telco-customer-churn-prediction",
 			demo: "",
@@ -267,7 +283,7 @@ export const projects: Array<Project> = [
 	{
 		title: "EZ Cart",
 		description:
-			"React-based ecommerce app enabling product browsing, detailed views, and cart and wishlist management, with Material-UI and Axios integration.",
+			"An ecommerce application built with React, featuring product browsing, detailed item views, and comprehensive cart and wishlist management. Integrated with Material-UI and Axios for a smooth and responsive user experience.",
 		links: {
 			github: "https://github.com/justine-george/EZCart-Web",
 			demo: "",
@@ -277,7 +293,7 @@ export const projects: Array<Project> = [
 	{
 		title: "Vegetable Price Catalog Generator for Local Businesses",
 		description:
-			"Multilingual catalogue generator for local Kerala businesses, enabling effortless creation of PNG catalogues with dynamic pricing and vegetable selection.",
+			"A multilingual catalogue generator tailored for local Kerala businesses, enabling the creation of dynamic PNG catalogues with customizable pricing and vegetable selection.",
 		links: {
 			github: "https://github.com/justine-george/store-catalogue-graphic-generator",
 			demo: "",
@@ -287,7 +303,7 @@ export const projects: Array<Project> = [
 	{
 		title: "Indian Judiciary ML Tool for NLP-Driven Case Analysis and Bilingual Translation",
 		description:
-			"Machine learning interface for the Indian Judicial System, featuring NLP applications like case analysis, centralized databases, and bilingual translation.",
+			"A machine learning tool designed for the Indian Judicial System, employing NLP techniques for case analysis and offering bilingual translation capabilities to support legal professionals in their work.",
 		links: {
 			github: "https://github.com/justine-george/Application-of-NLP-in-Indian-Judiciary",
 			demo: "",
