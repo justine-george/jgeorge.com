e3ae90c48b0f0b787d64e05e4d90642fed6a1084
Justine George <justinegeo96@gmail.com>
2024-09-23 16:12:08 -0700

    Update [slug].png.ts

--- src/pages/og-image/[slug].png.ts
+++ src/pages/og-image/[slug].png.ts
@@ -37,26 +37,11 @@ const markup = (title: string, pubDate: string) =>
 		</div>
 		<div tw="flex items-center justify-between w-full p-10 border-t border-[#2bbc89] text-xl">
 			<div tw="flex items-center">
-				<img src="/192x192.png" alt="Logo" tw="w-12 h-12" />
+				<img src="https://www.jgeorge.dev/192x192.png" alt="Logo" tw="w-12 h-12" />
 				<p tw="ml-3 font-semibold">${siteConfig.title}</p>
 			</div>
 		</div>
 	</div>`;
-
-// const markup = (title: string, pubDate: string) =>
-// 	html`<div tw="flex flex-col w-full h-full bg-[#1d1f21] text-[#c9cacc]">
-// 		<div tw="flex flex-col flex-1 w-full p-10 justify-center">
-// 			<p tw="text-2xl mb-6">${pubDate}</p>
-// 			<h1 tw="text-6xl font-bold leading-snug text-white">${title}</h1>
-// 		</div>
-// 		<div tw="flex items-center justify-between w-full p-10 border-t border-[#2bbc89] text-xl">
-// 			<div tw="flex items-center">
-// 				<img src="/192x192.png" alt="Logo" tw="w-12 h-12" />
-// 				<p tw="ml-3 font-semibold">${siteConfig.title}</p>
-// 			</div>
-// 			<p>by ${siteConfig.author}</p>
-// 		</div>
-// 	</div>`;
 
 export async function GET({ params: { slug } }: APIContext) {
 	const post = await getEntryBySlug("post", slug!);
